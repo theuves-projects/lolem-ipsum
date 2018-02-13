215746c69902c939eaa215407886774a369c11fe
Matheus Alves <theuves@outlook.com>
2018-02-13 20:56:44 -0300

    cria funcionalidades

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-"use strict";
+"use strict"
 
-import "./css/style.css";
+import loremIpsum from "lorem-ipsum"
+import cebolinha from "cebolinha"
+import "./css/style.css"
 
+function createText(count) {
+  return cebolinha(loremIpsum({
+    count: count,
+    units: "paragraphs",
+    paragraphLowerBound: 12
+  }))
+}
+
+function addText(selector, text) {
+  document.querySelector(selector).value = text
+}
+
+function getParagraphs() {
+  return document.querySelector("#paragraphs").value
+}
+
+function generateText() {
+  addText("#text", createText(getParagraphs()))
+}
+
+window
+  .addEventListener("load", generateText)
+
+document
+  .querySelector("#generate")
+  .addEventListener("click", generateText)
