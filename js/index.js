1e8ba8d9fa867c34951d21ec0044e2bbd18addd7
Matheus Alves <theuves@outlook.com>
2018-02-14 02:58:12 -0300

    deixa 'lorem ipsum...' em todos os texto
    ...e faz outras melhorias no código

--- js/index.js
+++ js/index.js
@@ -1,33 +1,36 @@
-"use strict"
+"use strict";
 
-import loremIpsum from "lorem-ipsum"
-import cebolinha from "cebolinha"
-import "normalize.css"
-import "../css/index.css"
+import loremIpsum from "lorem-ipsum";
+import cebolinha from "cebolinha";
+import "normalize.css";
+import "../css/index.css";
 
 function createText(count) {
-  return cebolinha(loremIpsum({
+  const init = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+  const randomText = loremIpsum({
     count: count,
     units: "paragraphs",
     paragraphLowerBound: 12
-  }))
+  });
+
+  const text = `${init} ${randomText}`;
+
+  return cebolinha(text);
 }
 
 function addText(selector, text) {
-  document.querySelector(selector).value = text
+  document.querySelector(selector).value = text;
 }
 
 function getParagraphs() {
-  return document.querySelector("#paragraphs").value
+  return document.querySelector("#paragraphs").value;
 }
 
 function generateText() {
-  addText("#text", createText(getParagraphs()))
+  addText("#text", createText(getParagraphs()));
 }
 
-window
-  .addEventListener("load", generateText)
+window.addEventListener("load", generateText);
 
-document
-  .querySelector("#generate")
-  .addEventListener("click", generateText)
+document.querySelector("#generate").addEventListener("click", generateText);
